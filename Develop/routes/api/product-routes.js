fda86572762f298eeb9f319a0bc8478a1969cabd
KeanuFord <KeanuF2017@gmail.com>
2024-04-29 01:07:36 -0700

    Updated rountes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -92,8 +92,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // TODO: delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
